66f39e00de8e73d03e3844826419ada9c7400738
Gambya <robertolopesramos@gmail.com>
2014-05-08 23:04:02 -0300

    RC

---
+++ PanFASE/PanFASE/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanFASE
+{
+    abstract public class Pedido
+    {
+        public int Codigo { get; set; }
+        public Cliente Clientes { get; set; }
+        public Funcionario Funcionarios { get; set; }
+        public Pedido()
+        {
+            this.Clientes = new Cliente();
+            this.Funcionarios = new Funcionario();
+        }
+    }
+}
